5c74e67b99ee8cd732a9cd00f9566bd4f61a94ac
Adam <Plindzek@users.noreply.github.com>
2019-12-06 13:05:37 +0100

    move code to new file and refactor access to moved variables

--- Koszt paliwa Swing/src/main/java/consoleVersion/Trip.java
+++ Koszt paliwa Swing/src/main/java/consoleVersion/Trip.java
@@ -4,76 +4,44 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Trip {
-	private double spalanieNa100kmLPG;
-	public double getspalanieNa100kmLPG() {
-		return spalanieNa100kmLPG;
-	}
-	private double spalanieNa100kmPB95;
-	public double getspalanieNa100kmPB95() {
-		return spalanieNa100kmPB95;
-	}
-	private double cenaLPG;
-	public double getcenaLPG() {
-		return cenaLPG;
-	}
-	private double cenaPB95;
-	public double getcenaPB95() {
-		return cenaPB95;
-	}
-	private double kmNaLPG;
-	public double getkmNaLPG() {
-		return kmNaLPG;
-	}
-	private double kmNaPB95;
-	public double getkmNaPB95() {
-		return kmNaPB95;
-	}
 
-	Trip() {
-		Scanner scan = new Scanner(System.in); //uzytkownik przypisuje zmiennym wartoœci wprowadzaj¹c je w konsoli
-		boolean isNotCorrect = true;
+    public static void main(String[] args) {
 
-		do {
-			try {
-				System.out.println(
-						"Podaj szacowane œrednie spalanie benzyny (w litrach/100km) w Twoim samochodzie \nwez pod uwagê charakter trasy, któr¹ chcesz pokonaæ:");
-				spalanieNa100kmPB95 = scan.nextDouble();
-				System.out.println(
-						"Podaj szacowane œrednie spalanie LPG (w litrach/100km) w Twoim samochodzie \nwez pod uwagê charakter trasy, któr¹ chcesz pokonaæ:");
-				spalanieNa100kmLPG = scan.nextDouble();
-				System.out.println("Ile kosztuje litr benzyny?");
-				cenaPB95 = scan.nextDouble();
-				System.out.println("Ile kosztuje litr LPG?");
-				cenaLPG = scan.nextDouble();
-				System.out.println("Ile km przejedziesz na LPG?");
-				kmNaLPG = scan.nextDouble();
-				System.out.println("Ile km przejedziesz na benzynie?");
-				kmNaPB95 = scan.nextDouble();
+	Scanner scan = new Scanner(System.in);
+	boolean isNotCorrect = true;
+	FuelCost cost = new FuelCost();
 
-				isNotCorrect = false;
-			} catch (InputMismatchException moje) {
+	do {
+	    try {
+		System.out.println(
+			"Podaj szacowane Å›rednie spalanie benzyny (w litrach/100km) w Twoim samochodzie \nwez pod uwagÄ™ charakter trasy, ktÃ³rÄ… chcesz pokonaÄ‡:");
+		cost.pb95On100km = scan.nextDouble();
+		System.out.println(
+			"Podaj szacowane Å›rednie spalanie LPG (w litrach/100km) w Twoim samochodzie \nwez pod uwagÄ™ charakter trasy, ktÃ³rÄ… chcesz pokonaÄ‡:");
+		cost.lpgOn100km = scan.nextDouble();
+		System.out.println("Ile kosztuje litr benzyny?");
+		cost.pb95Price = scan.nextDouble();
+		System.out.println("Ile kosztuje litr LPG?");
+		cost.lpgPrice = scan.nextDouble();
+		System.out.println("Ile km przejedziesz na LPG?");
+		cost.kmOnLPG = scan.nextDouble();
+		System.out.println("Ile km przejedziesz na benzynie?");
+		cost.kmOnPB95 = scan.nextDouble();
 
-				moje.printStackTrace();
-				System.err.println("Niepoprawny format danych, wprowadz dane ponownie");
-				scan.nextLine();
-			}
-		} while (isNotCorrect);
-		scan.close();
-		
-	}
-	public static void main(String[] args) {
-		Trip myTrip = new Trip();
-		System.out.println("Cena LPG za 1 litr: " + myTrip.getcenaLPG() + " PLN");
-		System.out.println("Cena benzyny za 1 litr: " + myTrip.getcenaPB95() + " PLN");
-		System.out.println(myTrip.getkmNaLPG() + " kilometrów na LPG");
-		System.out.println(myTrip.getkmNaPB95() + " kilometrów na benzynie");
-		System.out.println(myTrip.getspalanieNa100kmLPG() + " - spalanie LPG na 100km");
-		System.out.println(myTrip.getspalanieNa100kmPB95() + " - spalanie benzyny na 100km");
-		System.out.println("Koszt podró¿y wyniesie " + myTrip.fuelCost() + " PLN");
-	}
-	double fuelCost() {
-		double fuelCost = (Math.round((spalanieNa100kmLPG * cenaLPG * kmNaLPG / 100 + spalanieNa100kmPB95 * cenaPB95 * kmNaPB95 / 100)* 1000.0))/1000.0;
-		return fuelCost;
-	}
-	
+		isNotCorrect = false;
+	    } catch (InputMismatchException e) {
+
+		System.out.println("Niepoprawny format danych, wprowadz ponownie\n");
+		scan.next();
+	    }
+	} while (isNotCorrect);
+	scan.close();
+
+	System.out.println("Cena LPG za 1 litr: " + cost.lpgPrice + " PLN\n" + "Cena benzyny za 1 litr: "
+		+ cost.pb95Price + " PLN\n" + cost.kmOnLPG + " kilometrÃ³w na LPG\n" + cost.kmOnPB95
+		+ " kilometrÃ³w na benzynie\n" + cost.lpgOn100km + " - spalanie LPG na 100km\n" + cost.pb95On100km
+		+ " - spalanie benzyny na 100km\n" + "Koszt podrÃ³Å¼y wyniesie " + cost.calculateCost() + " PLN");
+
+    }
+
 }
