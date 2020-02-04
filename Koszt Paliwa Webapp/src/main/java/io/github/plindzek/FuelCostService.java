29cac4bd6f27dc57f8a55d2b862146493da495b0
Adam <Plindzek@users.noreply.github.com>
2020-02-04 17:47:31 +0100

    create service to calculate fuel cost

---
+++ Koszt Paliwa Webapp/src/main/java/io/github/plindzek/FuelCostService.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package io.github.plindzek;
+
+/**
+ * @author Adam
+ *
+ */
+public class FuelCostService {
+    private double lpgPrice;
+    private double pbPrice;
+    private double kmOnLpg;
+    private double kmOnPb;
+    private double cost;
+
+//    public double calculateFuelCost() {
+//	cost =  (Math.round((getLpgOn100Km() *getLpgPrice()* getKmOnLpg() / 100 + getPbOn100Km() * getPbPrice()
+//		* getKmOnPb() / 100) * 100.0))
+//		/ 100.0;
+
+}
