eb991c3e27a3c79af95eabacf7b61e781ba321ec
Adam <Plindzek@users.noreply.github.com>
2020-02-04 17:48:34 +0100

    create util class to manage Hibernate sessions

---
+++ Koszt Paliwa Webapp/src/main/java/io/github/plindzek/HibernateUtil.java
@@ -0,0 +1,40 @@
+package io.github.plindzek;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+/**
+ * util class ensure static methods to create and close Hibernate sessions
+ *
+ */
+class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private HibernateUtil() {
+    }
+
+    static SessionFactory getSessionFactory() { return sessionFactory; }
+
+    private static SessionFactory buildSessionFactory() {
+	// A SessionFactory is set up once for an application!
+	final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+		.configure() // configures settings from hibernate.cfg.xml
+		.build();
+	try {
+	    return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
+	} catch (Exception e) {
+
+	    StandardServiceRegistryBuilder.destroy(registry);
+	    throw e;
+	}
+    }
+
+    static void close() {
+		if ( sessionFactory != null ) {
+			sessionFactory.close();
+		}
+}
+}
