74efd70709b683f669a96f88049b362ca2955156
Adam <Plindzek@users.noreply.github.com>
2020-02-19 18:33:44 +0100

    change package

---
+++ Koszt Paliwa Webapp/src/main/java/io/github/plindzek/Fuelcost/Trip.java
@@ -0,0 +1,32 @@
+package io.github.plindzek.fuelcost;
+
+class Trip {
+
+    private double kmOnLpg, kmOnPb, kmOnOn;
+
+
+    public double getKmOnLpg() {
+        return kmOnLpg;
+    }
+
+    public void setKmOnLpg(double kmOnLpg) {
+        this.kmOnLpg = kmOnLpg;
+
+    }
+
+    public double getKmOnPb() {
+        return kmOnPb;
+    }
+
+    public void setKmOnPb(double kmOnPb) {
+        this.kmOnPb = kmOnPb;
+    }
+
+    public double getKmOnOn() {
+        return kmOnOn;
+    }
+
+    public void setKmOnOn(double kmOnOn) {
+        this.kmOnOn = kmOnOn;
+    }
+}
