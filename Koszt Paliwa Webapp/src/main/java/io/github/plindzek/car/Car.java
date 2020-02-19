9fe25939d6315d51296f4b70814b941bc8272b7e
Adam <Plindzek@users.noreply.github.com>
2020-02-19 18:38:06 +0100

    change name of table nad add two fields

--- Koszt Paliwa Webapp/src/main/java/io/github/plindzek/car/Car.java
+++ Koszt Paliwa Webapp/src/main/java/io/github/plindzek/car/Car.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package io.github.plindzek.car;
 
@@ -15,15 +15,17 @@ import org.hibernate.annotations.GenericGenerator;
  *
  */
 @Entity
-@Table(name = "Car")
+@Table(name = "cars")
 public class Car {
-    
+
     @Id
     @GeneratedValue(generator = "inc")
     @GenericGenerator(name = "inc", strategy = "increment")
     private Integer id;
     private String name;
     private boolean lpgPowered;
+    private boolean pbPowered;
+    private boolean onPowered;
     private double onOn100Km;
     private double lpgOn100Km;
     private double pbOn100Km;
@@ -36,31 +38,71 @@ public class Car {
 
     @Override
     public String toString() {
-	return "Car [" + name + "]";
+        return "Car [" + name + "]";
     }
 
-    public Integer getId() { return id; }
+    public boolean isPbPowered() {
+        return pbPowered;
+    }
 
-    public void setId(Integer id) { this.id = id; }
+    public void setPbPowered(boolean pbPowered) {
+        this.pbPowered = pbPowered;
+    }
 
-    public String getName() { return name; }
+    public boolean isOnPowered() {
+        return onPowered;
+    }
 
-    public void setName(String name) { this.name = name; }
+    public void setOnPowered(boolean onPowered) {
+        this.onPowered = onPowered;
+    }
 
-    public boolean isLpgPowered() { return lpgPowered; }
+    public Integer getId() {
+        return id;
+    }
 
-    public void setLpgPowered(boolean lpgPowered) { this.lpgPowered = lpgPowered; }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-    public double getOnOn100Km() { return onOn100Km; }
+    public String getName() {
+        return name;
+    }
 
-    public void setOnOn100Km(double onOn100Km) { this.onOn100Km = onOn100Km; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public double getLpgOn100Km() { return lpgOn100Km; }
+    public boolean isLpgPowered() {
+        return lpgPowered;
+    }
 
-    public void setLpgOn100Km(double lpgOn100Km) { this.lpgOn100Km = lpgOn100Km; }
+    public void setLpgPowered(boolean lpgPowered) {
+        this.lpgPowered = lpgPowered;
+    }
 
-    public double getPbOn100Km() { return pbOn100Km; }
+    public double getOnOn100Km() {
+        return onOn100Km;
+    }
 
-    public void setPbOn100Km(double pbOn100Km) { this.pbOn100Km = pbOn100Km; }
+    public void setOnOn100Km(double onOn100Km) {
+        this.onOn100Km = onOn100Km;
+    }
+
+    public double getLpgOn100Km() {
+        return lpgOn100Km;
+    }
+
+    public void setLpgOn100Km(double lpgOn100Km) {
+        this.lpgOn100Km = lpgOn100Km;
+    }
+
+    public double getPbOn100Km() {
+        return pbOn100Km;
+    }
+
+    public void setPbOn100Km(double pbOn100Km) {
+        this.pbOn100Km = pbOn100Km;
+    }
 
 }
