8e2f742e31d8307baed4e071e234773ab0decae4
Adam <Plindzek@users.noreply.github.com>
2020-02-19 18:38:38 +0100

    add crud methods

--- Koszt Paliwa Webapp/src/main/java/io/github/plindzek/car/CarRepository.java
+++ Koszt Paliwa Webapp/src/main/java/io/github/plindzek/car/CarRepository.java
@@ -13,20 +13,18 @@ import java.util.Optional;
  * contain methods to execute CRUD operations on car table
  *
  */
-class CarRepository {
+public class CarRepository {
 
 	List<Car> findAll() {
 		var session = HibernateUtil.getSessionFactory().openSession();
 		var transaction = session.beginTransaction();
-		var result = session.createQuery("from Car", Car.class).list();
+		var result = session.createQuery("from cars", Car.class).list();
 
 		transaction.commit();
 		session.close();
 		return (result);
 
 	}
-
-
     Optional<Car> findById(Integer id) {
 
 	var session = HibernateUtil.getSessionFactory().openSession();
@@ -38,4 +36,53 @@ class CarRepository {
 	return Optional.ofNullable(result);
 
     }
+	public Optional<Car> findByName(String name) {
+
+		var session = HibernateUtil.getSessionFactory().openSession();
+		var transaction = session.beginTransaction();
+
+		var result = session.get(Car.class, name);
+		transaction.commit();
+		session.close();
+		return Optional.ofNullable(result);
+
+	}
+	 Car updateCar(Integer id, String newName) {
+		Car result = null;
+		var session = HibernateUtil.getSessionFactory().openSession();
+		var transaction = session.beginTransaction();
+		try {
+			result = session.get(Car.class, id);
+			result.setName(newName);
+			transaction.commit();
+		} catch (Exception e) {
+			transaction.rollback();
+		}
+		session.close();
+		return result;
+	}
+
+
+
+	Car addCar(Car newCar) {
+
+		var session = HibernateUtil.getSessionFactory().openSession();
+		var transaction = session.beginTransaction();
+
+		try {
+			session.persist("cars", newCar);
+
+			transaction.commit();
+
+		} catch (Exception e) {
+			transaction.rollback();
+		}
+		session.close();
+
+		return newCar;
+	}
+
+
+	void deleteCar(){}
+
 }
