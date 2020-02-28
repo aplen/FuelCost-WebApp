efcc102a62923b9e8c1a409adedb1dfa401d9bad
Adam <Plindzek@users.noreply.github.com>
2020-02-28 19:30:12 +0100

    add variables

--- Koszt Paliwa Webapp/src/main/java/io/github/plindzek/Fuelcost/FuelCostServlet.java
+++ Koszt Paliwa Webapp/src/main/java/io/github/plindzek/Fuelcost/FuelCostServlet.java
@@ -1,6 +1,5 @@
 package io.github.plindzek.fuelcost;
 
-import io.github.plindzek.car.Car;
 import io.github.plindzek.car.CarRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,14 +19,13 @@ import java.io.IOException;
 public class FuelCostServlet extends HttpServlet {
 
     private final Logger logger = LoggerFactory.getLogger(io.github.plindzek.fuelcost.FuelCostServlet.class);
-    private static final String KM_ON_LPG_PARAM = "kmOnLpg";
-    private static final String KM_ON_PB_PARAM = "kmOnPb";
-    private static final String KM_ON_ON_PARAM = "kmOnOn";
+    private static final String KM_ON_LPG_PARAM = "kmOnLpgValue";
+    private static final String KM_ON_PB_PARAM = "kmOnPbValue";
+    private static final String KM_ON_ON_PARAM = "kmOnOnValue";
+    private static final String PRICE_LPG_PARAM = "lpgPriceValue";
+    private static final String PRICE_PB_PARAM = "pbPriceValue";
+    private static final String PRICE_ON_PARAM = "onPriceValue";
 
-    /**
-     * define references needed to handle response (eg. service, mapper or
-     * repository)
-     */
     private FuelCostService fuelCostService;
     private CarRepository repository;
 
@@ -35,11 +33,12 @@ public class FuelCostServlet extends HttpServlet {
      * servlet container needs it
      */
     public FuelCostServlet() {
-        this(new FuelCostService());
+        this(new FuelCostService(), new CarRepository());
     }
 
-    FuelCostServlet(FuelCostService fuelCostService) {
+    FuelCostServlet(FuelCostService fuelCostService, CarRepository repository) {
         this.fuelCostService = fuelCostService;
+        this.repository = repository;
     }
 
     @Override
@@ -50,11 +49,22 @@ public class FuelCostServlet extends HttpServlet {
         var kmOnLpg = req.getParameter(KM_ON_LPG_PARAM);
         var kmOnPb = req.getParameter(KM_ON_PB_PARAM);
         var kmOnOn = req.getParameter(KM_ON_ON_PARAM);
+        var lpgPrice = req.getParameter(PRICE_LPG_PARAM);
+        var pbPrice = req.getParameter(PRICE_PB_PARAM);
+        var onPrice = req.getParameter(PRICE_ON_PARAM);
         var trip = new Trip();
         try {
-            trip.setKmOnPb(Double.parseDouble(kmOnPb));
-            trip.setKmOnLpg(Double.parseDouble(kmOnLpg));
-            trip.setKmOnOn(Double.parseDouble(kmOnOn));
+            //TODO
+            //przekazywany ma byc jsonem obiekt Trip oraz ID samochodu do pobrania
+
+            //trip.setKmOnPb(Double.parseDouble(kmOnPb));
+            //trip.setKmOnLpg(Double.parseDouble(kmOnLpg));
+            //trip.setKmOnOn(Double.parseDouble(kmOnOn));
+            //
+            //trip.setLpgPrice(Double.parseDouble(lpgPrice));
+            //trip.setPbPrice(Double.parseDouble(pbPrice));
+            //trip.setOnPrice(Double.parseDouble(onPrice));
+
         } catch (NumberFormatException | NullPointerException e) {
             //e.printStackTrace();
             resp.getWriter().println("wrong data format");
@@ -71,7 +81,7 @@ public class FuelCostServlet extends HttpServlet {
             resp.setContentType("text/html; charset=utf-8");
             //resp.setContentType("application/json; charset=utf-8");
             resp.getWriter().write((Double.toString(wynik)));
-        } catch (NumberFormatException|NullPointerException e) {
+        } catch (NumberFormatException | NullPointerException e) {
             logger.info("Wrong path: " + pathInfo);
         }
 
