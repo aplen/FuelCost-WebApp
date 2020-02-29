7e3d2787ac338ca3f3a15f3c10501ca458742171
Adam <Plindzek@users.noreply.github.com>
2020-02-29 17:20:31 +0100

    add json handle to make Trip object and calculate result

--- Koszt Paliwa Webapp/src/main/java/io/github/plindzek/Fuelcost/FuelCostServlet.java
+++ Koszt Paliwa Webapp/src/main/java/io/github/plindzek/Fuelcost/FuelCostServlet.java
@@ -1,9 +1,9 @@
 package io.github.plindzek.fuelcost;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.plindzek.car.CarRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,74 +19,44 @@ import java.io.IOException;
 public class FuelCostServlet extends HttpServlet {
 
     private final Logger logger = LoggerFactory.getLogger(io.github.plindzek.fuelcost.FuelCostServlet.class);
-    private static final String KM_ON_LPG_PARAM = "kmOnLpgValue";
-    private static final String KM_ON_PB_PARAM = "kmOnPbValue";
-    private static final String KM_ON_ON_PARAM = "kmOnOnValue";
-    private static final String PRICE_LPG_PARAM = "lpgPriceValue";
-    private static final String PRICE_PB_PARAM = "pbPriceValue";
-    private static final String PRICE_ON_PARAM = "onPriceValue";
-
     private FuelCostService fuelCostService;
     private CarRepository repository;
+    private ObjectMapper mapper;
 
     /*
      * servlet container needs it
      */
     public FuelCostServlet() {
-        this(new FuelCostService(), new CarRepository());
+        this(new FuelCostService(), new CarRepository(), new ObjectMapper());
     }
 
-    FuelCostServlet(FuelCostService fuelCostService, CarRepository repository) {
+    FuelCostServlet(FuelCostService fuelCostService, CarRepository repository, ObjectMapper mapper) {
         this.fuelCostService = fuelCostService;
         this.repository = repository;
+        this.mapper=mapper;
+
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        logger.info("Request got with parameters: " + req.getParameterMap());
-
-        var kmOnLpg = req.getParameter(KM_ON_LPG_PARAM);
-        var kmOnPb = req.getParameter(KM_ON_PB_PARAM);
-        var kmOnOn = req.getParameter(KM_ON_ON_PARAM);
-        var lpgPrice = req.getParameter(PRICE_LPG_PARAM);
-        var pbPrice = req.getParameter(PRICE_PB_PARAM);
-        var onPrice = req.getParameter(PRICE_ON_PARAM);
-        var trip = new Trip();
-        try {
-            //TODO
-            //przekazywany ma byc jsonem obiekt Trip oraz ID samochodu do pobrania
-
-            //trip.setKmOnPb(Double.parseDouble(kmOnPb));
-            //trip.setKmOnLpg(Double.parseDouble(kmOnLpg));
-            //trip.setKmOnOn(Double.parseDouble(kmOnOn));
-            //
-            //trip.setLpgPrice(Double.parseDouble(lpgPrice));
-            //trip.setPbPrice(Double.parseDouble(pbPrice));
-            //trip.setOnPrice(Double.parseDouble(onPrice));
-
-        } catch (NumberFormatException | NullPointerException e) {
-            //e.printStackTrace();
-            resp.getWriter().println("wrong data format");
-        }
+        var trip = mapper.readValue(req.getInputStream(), Trip.class);
 
         String pathInfo = req.getPathInfo();
         logger.info("Request got with path info  " + pathInfo);
+
         try {
+
             var id = Integer.valueOf(pathInfo.substring(1));
             var car = repository.findById(id).orElse(null);
-            //TODO
+
             double wynik = fuelCostService.calcCost(car, trip);
-            System.out.println(wynik);
             resp.setContentType("text/html; charset=utf-8");
-            //resp.setContentType("application/json; charset=utf-8");
             resp.getWriter().write((Double.toString(wynik)));
+
         } catch (NumberFormatException | NullPointerException e) {
             logger.info("Wrong path: " + pathInfo);
         }
 
     }
 }
-
-
-
