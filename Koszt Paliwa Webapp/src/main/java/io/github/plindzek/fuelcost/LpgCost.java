00ae22afeffdf19940bb8655139b4b5953d554fd
Adam <Plindzek@users.noreply.github.com>
2020-02-29 17:21:19 +0100

    notnull anotation

--- Koszt Paliwa Webapp/src/main/java/io/github/plindzek/fuelcost/LpgCost.java
+++ Koszt Paliwa Webapp/src/main/java/io/github/plindzek/fuelcost/LpgCost.java
@@ -2,6 +2,7 @@ package io.github.plindzek.fuelcost;
 
 import io.github.plindzek.car.Car;
 import io.github.plindzek.prices.FuelsPriceScrapper;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Adam
@@ -9,7 +10,7 @@ import io.github.plindzek.prices.FuelsPriceScrapper;
 class LpgCost implements FuelCost {
 
     @Override
-    public double calculateFuelCost(Car car, Trip trip) {
+    public double calculateFuelCost(@NotNull Car car, Trip trip) {
        return  ((Math.round((car.getLpgOn100Km() * trip.getLpgPrice()* trip.getKmOnLpg() / 100) * 100.0))
        + (Math.round((car.getPbOn100Km() * trip.getPbPrice()* trip.getKmOnPb() / 100) * 100.0)))
                 / 100.0;
