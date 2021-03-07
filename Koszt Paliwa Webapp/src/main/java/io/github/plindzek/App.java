d76e3da5528cecc11a0ddbb8a010fa2f688c986b
Adam Plencler <aplencler@gmail.com>
2021-03-07 22:50:29 +0100

    update

--- Koszt Paliwa Webapp/src/main/java/io/github/plindzek/App.java
+++ Koszt Paliwa Webapp/src/main/java/io/github/plindzek/App.java
@@ -19,27 +19,27 @@ import org.eclipse.jetty.webapp.WebXmlConfiguration;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        /** create and configure handler */
+        /* create and configure handler */
         var webapp = new WebAppContext();
 
-        /**
-         * this line create servlet but without annotations handling we need
-         * annotations, so its off
-         * webapp.addServlet(HelloServlet.class, "/api/*");
+        /*
+          this line create servlet but without annotations handling we need
+          annotations, so its off
+          webapp.addServlet(HelloServlet.class, "/api/*");
          */
 
-        /**
-         * this line allow to change static files in webapp folder without server
-         * restart
+        /*
+          this line allow to change static files in webapp folder without server
+          restart
          */
         webapp.setInitParameter("org.eclipse.jetty.servlet.Default.maxCachedFiles", "0");
 
         webapp.setResourceBase("src/main/webapp");
         webapp.setContextPath("/");
 
-        /**
-         * configuration from stackoverflow "webapp context configuration embedded
-         * servlet 3.1"
+        /*
+          configuration from stackoverflow "webapp context configuration embedded
+          servlet 3.1"
          */
         webapp.setConfigurations(new Configuration[]{new AnnotationConfiguration(), new WebInfConfiguration(),
                 new WebXmlConfiguration(), new MetaInfConfiguration(), new FragmentConfiguration(),
@@ -49,7 +49,7 @@ public class App {
         var server = new Server(8080);
         server.setHandler(webapp);
 
-        /** closing Hibernate session when closing server */
+        /* closing Hibernate session when closing server */
         server.addLifeCycleListener(new AbstractLifeCycle.AbstractLifeCycleListener() {
             @Override
             public void lifeCycleStopped(LifeCycle event) {
